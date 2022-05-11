2eff34d31591858efb4c833f7bfac542fab5e6af
Chong Chen <chenchong.blog@gmail.com>
2022-05-11 09:04:51 -0400

    Windows10

--- app/src/main/java/com/qizhou/myprofile/SettingsActivity.java
+++ app/src/main/java/com/qizhou/myprofile/SettingsActivity.java
@@ -15,7 +15,7 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void onConfirmClicked(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
